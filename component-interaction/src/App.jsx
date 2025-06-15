71fc888c42fced301711ccc4395f461c7e06b372
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2025-06-15 02:01:40 +0000

    change app.jsx

--- component-interaction/src/App.jsx
+++ component-interaction/src/App.jsx
@@ -1,18 +1,18 @@
-// import Dashboard from "./components/Dashboard"
-import { CartProvider } from './context/CartContext';
-import AddToCartButton from './components/AddToCartButton';
-import CartCounter from './components/CartCounter';
+import Dashboard from "./components/Dashboard"
+// import { CartProvider } from './context/CartContext';
+// import AddToCartButton from './components/AddToCartButton';
+// import CartCounter from './components/CartCounter';
 
 function App() {
 
   return (
     <>
-      {/* <Dashboard /> */}
-      <CartProvider>
+      <Dashboard />
+      {/* <CartProvider>
         <h1>Shopping App</h1>
         <AddToCartButton />
         <CartCounter />
-      </CartProvider>
+      </CartProvider> */}
     </>
   )
 }
