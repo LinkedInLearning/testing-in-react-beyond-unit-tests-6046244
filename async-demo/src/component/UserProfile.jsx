256e8a33b4667d7724f64ad64109cc72c31505f4
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2025-06-18 01:06:35 +0000

    end of 03_02 movie

--- async-demo/src/component/UserProfile.jsx
+++ async-demo/src/component/UserProfile.jsx
@@ -1,33 +1,18 @@
 import { useEffect, useState } from 'react';
-// import { getUser } from '../api';
+import { getUser } from '../api';
 
 export default function UserProfile() {
   const [user, setUser] = useState(null);
   const [status, setStatus] = useState('loading');
 
   useEffect(() => {
-    fetch('https://api.example.com/user')
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('Network error');
-        }
-        return res.json();
-      })
+    getUser()
       .then((data) => {
         setUser(data);
         setStatus('success');
       })
       .catch(() => setStatus('error'));
   }, []);
-
-  // useEffect(() => {
-  //   getUser()
-  //     .then((data) => {
-  //       setUser(data);
-  //       setStatus('success');
-  //     })
-  //     .catch(() => setStatus('error'));
-  // }, []);
 
   return status === 'loading' ? (
     <p>Loading user...</p>
